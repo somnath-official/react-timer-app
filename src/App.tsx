c22625a33af6d1d86c91b06857c896074f024012
somnathsardar <githubsomnath1996@gmail.com>
2023-12-02 23:01:38 +0530

    Setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { useSelector } from 'react-redux'
 import './App.css'
 import Timer from './components/timer/Timer'
+import { RootState } from './store'
 
 function App() {
+  const timerData = useSelector((state: RootState) => state.timer)
+
   return (
     <>
       <div className='timer-container'>
-        <Timer timeToRune={30} />
-        <Timer timeToRune={10} />
-        <Timer timeToRune={15} />
-        <Timer timeToRune={20} />
-        <Timer timeToRune={20} />
-        <Timer timeToRune={20} />
+        {
+          timerData.timers.length
+          ? timerData.timers.map((timer) => {
+              return <Timer timeToRune={timer.seconds} key={timer.id}/>
+            })
+          : <div className='no-timer-created'>No timer created</div>
+        }
       </div>
       {/* <div className='timer-action-container'>
         <button onClick={() => setStartTimer(true)}>Start</button>
---
+++ src/store/features/timerSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+interface Timer {
+  id: number
+  seconds: number
+}
+
+export interface TimerState {
+  startAll: boolean
+  timers: Timer[]
+}
+
+const initialState: TimerState = 
+{
+  startAll: false,
+  timers: []
+}
+
+export const counterSlice = createSlice({
+  name: 'timer',
+  initialState,
+  reducers: {
+    createNewTimer: (state, action: PayloadAction<number>) => {
+      state.timers.push({id: + new Date(), seconds: action.payload })
+    },
+    deleteTimer: (state, action: PayloadAction<number>) => {
+      state.timers = state.timers.filter((item) => item.id !== action.payload)
+    },
+    startAlltimer: (state) => {
+      state.startAll = true
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { createNewTimer, deleteTimer, startAlltimer } = counterSlice.actions
+
+export default counterSlice.reducer
