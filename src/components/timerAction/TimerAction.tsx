1db75ffad8434c094334029bec701e86623a60af
somnathsardar <githubsomnath1996@gmail.com>
2023-12-03 11:14:04 +0530

    Disabled button if no timer is created.

--- src/components/timerAction/TimerAction.tsx
+++ src/components/timerAction/TimerAction.tsx
@@ -1,13 +1,41 @@
+import { useSelector } from 'react-redux'
 import './TimerAction.css'
+import { RootState } from '../../store'
 
 const TimerAction = () => {
+  const timerData = useSelector((state: RootState) => state.timer)
+
   return (
     <div className='timer-action-container'>
-      <button className='btn btn-success'>Add New</button>
-      <button className='btn btn-primary'>Start All</button>
-      <button className='btn btn-secondary'>Pause All</button>
-      <button className='btn btn-dark' disabled>Reset All</button>
-      <button className='btn btn-danger'>Delete All</button>
+      <button
+        className='btn btn-success'
+      >
+        Add New
+      </button>
+      <button
+        className='btn btn-primary'
+        disabled={!timerData.timers.length}
+      >
+        Start All
+      </button>
+      <button
+        className='btn btn-secondary'
+        disabled={!timerData.timers.length}
+      >
+        Pause All
+      </button>
+      <button
+        className='btn btn-dark'
+        disabled={!timerData.timers.length}
+      >
+        Reset All
+      </button>
+      <button
+        className='btn btn-danger'
+        disabled={!timerData.timers.length}
+      >
+        Delete All
+      </button>
     </div>
   )
 }
