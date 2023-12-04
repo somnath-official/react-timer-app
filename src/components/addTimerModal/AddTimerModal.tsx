443e131186276092705b5035ef5661b294be727d
somnathsardar <githubsomnath1996@gmail.com>
2023-12-04 11:37:36 +0530

    Added moda to add new timer

---
+++ src/components/addTimerModal/AddTimerModal.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useRef, useState } from 'react'
+import './AddTimerModal.css'
+
+interface AddTimerModalProps {
+  show: boolean
+  toggleModal: (d: boolean) => void
+}
+
+const AddTimerModal = ({show, toggleModal}: AddTimerModalProps) => {
+  const modalRef = useRef(null)
+  const [seconds, setSeconds] = useState()
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        toggleModal(false)
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  })
+
+  useEffect(() => {
+    return () => {
+      setSeconds(undefined)
+    }
+  }, [show])
+
+  function addTimerData() {}
+
+  return (
+    show
+    ? <div className="modal">
+        <div className="modal-content" ref={modalRef}>
+          <div className='modal-header'>
+            <h3>Add new timer</h3>
+          </div>
+          <div className='modal-body'>
+            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+              <label className="custom-field transit">
+                <input
+                  type="number"
+                  placeholder=" "
+                  value={seconds}
+                  onInput={(e) => {
+                    setSeconds(e.target.value)
+                  }}
+                />
+                <span className="placeholder">Enter time in seconds</span>
+              </label>
+              <div
+                style={{
+                  width: '100%',
+                  marginLeft: '20px',
+                  display: 'flex',
+                  justifyContent: 'space-evenly',
+                  alignItems: 'center',
+                }}
+              >
+                <button
+                  className='btn btn-sm btn-success'
+                  onClick={addTimerData}
+                  disabled={!seconds}
+                >
+                  Add
+                </button>
+                <button
+                  className='btn btn-sm btn-danger'
+                  onClick={() => toggleModal(false)}
+                >
+                  cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    : ''
+  )
+}
+
+export default AddTimerModal
--- src/components/timerAction/TimerAction.tsx
+++ src/components/timerAction/TimerAction.tsx
@@ -2,47 +2,59 @@ import { useDispatch, useSelector } from 'react-redux'
 import './TimerAction.css'
 import { RootState } from '../../store'
 import { deleteAllTimer, pauseAllTimer, resetAllTimer, startAlltimer } from '../../store/features/timerSlice'
+import AddTimerModal from '../addTimerModal/AddTimerModal'
+import { useState } from 'react'
 
 const TimerAction = () => {
   const timerData = useSelector((state: RootState) => state.timer)
   const dispatch = useDispatch()
+  const [showModal, setShowModal] = useState(false)
+
+  function toggleModal() {
+    setShowModal((prev) => !prev)
+  }
 
   return (
-    <div className='timer-action-container'>
-      <button
-        className='btn btn-success'
-      >
-        Add New
-      </button>
-      <button
-        className='btn btn-primary'
-        disabled={!timerData.timers.length}
-        onClick={() => dispatch(startAlltimer())}
-      >
-        Start All
-      </button>
-      <button
-        className='btn btn-secondary'
-        disabled={!timerData.timers.length}
-        onClick={() => dispatch(pauseAllTimer())}
-      >
-        Pause All
-      </button>
-      <button
-        className='btn btn-dark'
-        disabled={!timerData.timers.length}
-        onClick={() => dispatch(resetAllTimer(true))}
-      >
-        Reset All
-      </button>
-      <button
-        className='btn btn-danger'
-        disabled={!timerData.timers.length}
-        onClick={() => dispatch(deleteAllTimer())}
-      >
-        Delete All
-      </button>
-    </div>
+    <>
+      <div className='timer-action-container'>
+        <button
+          className='btn btn-success'
+          onClick={toggleModal}
+        >
+          Add New
+        </button>
+        <button
+          className='btn btn-primary'
+          disabled={!timerData.timers.length}
+          onClick={() => dispatch(startAlltimer())}
+        >
+          Start All
+        </button>
+        <button
+          className='btn btn-secondary'
+          disabled={!timerData.timers.length}
+          onClick={() => dispatch(pauseAllTimer())}
+        >
+          Pause All
+        </button>
+        <button
+          className='btn btn-dark'
+          disabled={!timerData.timers.length}
+          onClick={() => dispatch(resetAllTimer(true))}
+        >
+          Reset All
+        </button>
+        <button
+          className='btn btn-danger'
+          disabled={!timerData.timers.length}
+          onClick={() => dispatch(deleteAllTimer())}
+        >
+          Delete All
+        </button>
+      </div>
+
+      <AddTimerModal show={showModal} toggleModal={toggleModal}/>
+    </>
   )
 }
 
